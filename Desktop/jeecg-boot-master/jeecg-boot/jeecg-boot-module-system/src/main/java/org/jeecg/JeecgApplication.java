10c11bac176300dea9deb7712bd41a781a9f8ad9
aspklman <562142139@qq.com>
2019-09-10 11:06:22 +0800

    修改主程序5

--- Desktop/jeecg-boot-master/jeecg-boot/jeecg-boot-module-system/src/main/java/org/jeecg/JeecgApplication.java
+++ Desktop/jeecg-boot-master/jeecg-boot/jeecg-boot-module-system/src/main/java/org/jeecg/JeecgApplication.java
@@ -15,7 +15,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 @SpringBootApplication
 public class JeecgApplication {
-
+//88888888
   public static void main(String[] args) throws UnknownHostException {
     //System.setProperty("spring.devtools.restart.enabled", "true");
 
